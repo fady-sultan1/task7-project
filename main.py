9d192a4721e23e2c6d5162c7fd770e7fd10f88b6
fady-sultan1 <ana0597846448@gmail.com>
2023-08-20 18:38:39 +0300

    2

---
+++ main.py
@@ -0,0 +1,81 @@
+
+# x = int(input('Input the number: '))
+# def print_number_of_rows():
+#     for y in range(x + 1):
+#         for z in range(1, y + 1):
+#             print(z, end=' ')
+#         print()
+# print_number_of_rows()
+
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# num = int(input("Enter number :"))
+# def sum_num():
+#     sum1 = 0
+#     for i in range(1, num + 1):
+#         sum1 += i
+#     return sum1
+# result = sum_num()
+# print("Sum is :", result)
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+# name = input("Enter your name : ")
+# def a():
+#     for i in range(len(name)):
+#         print(name[:i], end=" ")
+#         print()
+#     for v in range(len(name), 0, -1):
+#         print(name[:v], end=" ")
+#         print()
+# a()
+#
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+# n = input("input a word to revers: ")
+# def s():
+#     print(n[::-1])
+# s()
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+# def p():
+#     o = int(input('Enter range: '))
+#
+#     while o>=1:
+#         print(o, end=" ")
+#         o +=-1
+#     print()
+# p()
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+# def k():
+#     t = int(input('multiples of 5: '))
+#     while t<=50:
+#         print(t, end=" ")
+#         t +=5
+# k()
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+# def main():
+#     Limit_number = int(input("Enter the limit number: "))
+#     Target_number = int(input("Enter the target number: "))
+#     Max_display_on_screen = int(input("Enter the maximum outputs to display (Max-display-on-screen): "))
+#     count = 0
+#     current_number = Target_number
+#
+#     while current_number <= Limit_number and count < Max_display_on_screen:
+#         print(current_number, end=" ")
+#         count += 1
+#         current_number += Target_number
+#
+# if __name__ == "__main__":
+#     main()
